fd59381ca52a979f4bcf99e5449d2a2b6a74f2a5
mpho2024 <khanyatsatsi@gmail.com>
2024-04-16 16:34:16 +0200

    m

---
+++ quizApp/js/index.js
@@ -0,0 +1,33 @@
+var quiz= {
+    data:[
+        q: "what is the standard of distance between the target and archer in olympics? ",
+        o:[
+            "50 meter",
+            "70 meter",
+            "100 meter",
+            "120 meter",
+        ],
+        a:1
+    },
+    {
+    q: "which is the seven planet from the sun?",
+     o:[
+        "uranus",
+        "earth",
+        "pluto",
+        "mars",
+    ],
+    a:0
+},
+{
+    q: "which is the highest number on a standard roulette wheen",
+    o:[
+        "22",
+        "24",
+        "32",
+        "36",
+    ],
+    a:3
+},
+],
+hw
